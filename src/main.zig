c87071e022853cff6d85948b810127f40e8970da
Matthew Lance Fuller <matthewlancefuller@gmail.com>
2022-07-05 04:32:55 -0700

    Improve main.zig example

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,27 @@
 const std = @import("std");
+const eco = @import("eco");
+const io = std.io;
+
+fn f() {
+    const S = struct {
+        var x: isize = 0;
+    }
+    var i = 0;
+    var id = 0;
+    const stdout = io.getStdOut().writer();
+
+    x += 1;
+    id = x;
+    while (i < 10) {
+        try stdout.print("{d} {d}\n", .{id, i});
+        _ = eco.eco_yield();
+        i += 1;
+    }
+}
 
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
-    try stdout.print("Hello, {s}!\n", .{"world"});
+    eco.eco_init();
+    eco.eco_go(f);
+    eco.eco_go(f);
+    _ = eco.eco_return(1);
 }
